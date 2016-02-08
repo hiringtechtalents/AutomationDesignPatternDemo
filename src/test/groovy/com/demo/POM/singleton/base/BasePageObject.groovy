3c2cecabb26cbf468e83afc5221f6fbb80fbdbac
Sandeep Singh <s.sandeep79@ymail.com>
2016-02-08 11:17:38 +0530

    Added logback logging to the framework.
    Working for BasePageObject.groovy file for now. will add more soon.

--- src/test/groovy/com/demo/POM/singleton/base/BasePageObject.groovy
+++ src/test/groovy/com/demo/POM/singleton/base/BasePageObject.groovy
@@ -1,5 +1,6 @@
 package com.demo.POM.singleton.base
 
+import groovy.util.logging.Slf4j
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
@@ -7,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import org.testng.Assert
 
+@Slf4j
 abstract class BasePageObject {
 	protected def driver
 	protected def wait
@@ -14,10 +16,13 @@ abstract class BasePageObject {
 	private def config = FrameworkConfig.instance.config
 
 	public BasePageObject(WebDriver driver) {
+        log.info(String.format("Entered %s constructor", this.class.simpleName))
 		this.driver = driver;
 		wait = new WebDriverWait(this.driver, config.WEBDRIVERWAIT_TIMEOUT, config.WEBDRIVERWAIT_POLL)
-		
+
+        log.info("Calling isLoaded method to validate if the page is loaded.")
 		isLoaded();
+        log.info(String.format("Exiting %s constructor", this.class.simpleName))
 	}
 	
 	/**
@@ -28,6 +33,7 @@ abstract class BasePageObject {
 	protected abstract By getUniqueElement();
 	
 	protected void isLoaded() throws Error{
+        log.info("Entering isLoaded method")
 		//Define a list of WebElements that match the unique element locator for the page
         List<WebElement> uniqueElement = driver.findElements(getUniqueElement())
 
@@ -38,6 +44,7 @@ abstract class BasePageObject {
         // Wait until the unique element is visible in the browser and ready to use. This helps make sure the page is
         // loaded before the next step of the tests continue.
         wait.until(ExpectedConditions.visibilityOfElementLocated(getUniqueElement()))
+        log.info("Exiting isLoaded method.")
 	}
 
 }
